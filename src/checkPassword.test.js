5b15461c4061af74beeeb8e23d4cc1200af3f6cc
Yaroslav <yaroslavhohulia@gmail.com>
2026-07-10 10:02:43 +0300

    added tests

--- src/checkPassword.test.js
+++ src/checkPassword.test.js
@@ -7,9 +7,19 @@ describe(`Function 'checkPassword':`, () => {
     expect(checkPassword).toBeInstanceOf(Function);
   });
 
-  it(`should return boolean`, () => {});
+  it(`should return boolean`, () => {
+    expect(typeof checkPassword('Password1!')).toBe('boolean');
+  });
 
-  it(`should return 'true' for the valid password with 8 characters`, () => {});
+  it(`should return 'true' for the valid password with 8 characters`, () => {
+    expect(checkPassword('Password1!')).toBe(true);
+  });
 
-  // write more tests here
+  it(`should return 'false' for a short password without special characters (qwerty)`, () => {
+    expect(checkPassword('qwerty')).toBe(false);
+  });
+
+  it(`should return 'false' for a password that lacks numbers or sufficient length (Str@ng)`, () => {
+    expect(checkPassword('Str@ng')).toBe(false);
+  });
 });
